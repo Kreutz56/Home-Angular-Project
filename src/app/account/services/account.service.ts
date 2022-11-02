506c70977a5f1df3d582f756157bb7ae4c1284e0
Kreutz56 <guilherme_vg1@hotmail.com>
2022-11-02 08:06:36 -0300

    Account service created

---
+++ src/app/account/services/account.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { User } from "../models/user";
+import { Observable } from "rxjs";
+import { map, catchError } from "rxjs/operators"
+import { BaseService } from "src/app/services/base.service";
+
+
+@Injectable ()
+
+export class AccountService extends BaseService {
+
+    constructor (private http: HttpClient) { super(); }
+
+    registerUser(user: User) : Observable<User> {
+        let response = this.http
+            .post(this.UrlServiceV1 + 'new-account', user, this.getJsonHeader())
+            .pipe(
+                map(this.extractData),
+                catchError(this.serviceError)
+            );
+        return response;
+
+    }
+
+    loginUser(user: User) {
+
+    }
+
+}
