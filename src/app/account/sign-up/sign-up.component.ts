8bbe65591d62dc61637edc96657ea7cfb731da30
Kreutz56 <guilherme_vg1@hotmail.com>
2022-11-02 08:15:43 -0300

    SignUp component functions implemented

--- src/app/account/sign-up/sign-up.component.ts
+++ src/app/account/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { User } from "../models/user";
+import { AccountService } from "../services/account.service";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,37 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class SignUpComponent implements OnInit {
-  constructor() {}
+
+  signUpForm: FormGroup;
+  user: User;
+  constructor(private formBuilder: FormBuilder,
+    private accountService: AccountService) { }
 
   ngOnInit(): void {
-    
+    this.signUpForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+      confirmPassword: ['']
+    });
+  }
+
+  addAccount() {
+    if (this.signUpForm.dirty && this.signUpForm.valid) {
+      this.user = Object.assign({}, this.user, this.signUpForm.value)
+
+      this.accountService
+        .registerUser(this.user)
+        .subscribe(
+          success => { this.processSuccess(success) },
+          fail => { this.processFail(fail) }
+        )
+
+    }
+  }
+
+  processSuccess(success: User) {
+  }
+
+  processFail(fail: any) {
   }
 }
