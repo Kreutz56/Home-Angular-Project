c12cf124522c02ee8fa0a05bbb95dffef9845f5e
Kreutz56 <guilherme_vg1@hotmail.com>
2022-11-05 21:49:31 -0300

    Add market item component base implemented

---
+++ src/app/market/add-market-item/add-market-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { MarketItem } from "../models/MarketItem";
+import { MarketService } from "../services/market.service";
+
+@Component({
+    selector: 'app-market-item',
+    templateUrl: './add-market-item.component.html'
+})
+
+export class AddMarketItemComponent implements OnInit {
+
+    marketForm: FormGroup;
+    item: MarketItem;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private marketService: MarketService
+    ) {}
+
+    ngOnInit(): void {
+        this.marketForm = this.formBuilder.group({
+            marketItem: ['']
+        });
+    }
+
+    addMarketItem(item: MarketItem) {
+
+    }
+}
