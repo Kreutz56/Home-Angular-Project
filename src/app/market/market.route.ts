6beeca5f6622e94e166a36ab18526fd3d7923a4f
Kreutz56 <guilherme_vg1@hotmail.com>
2022-11-08 21:20:08 -0300

    Market app child routes implemented

--- src/app/market/market.route.ts
+++ src/app/market/market.route.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AddMarketItemComponent } from "./add-market-item/add-market-item.component";
 import { MarketAppComponent } from "./market.app.component";
 
 
 const marketRouterConfig: Routes = [
-    { path: '', component: MarketAppComponent },
+    { path: '', component: MarketAppComponent, children: [
+        { path: 'add', component: AddMarketItemComponent }
+    ] },
 ]
 
 @NgModule({
